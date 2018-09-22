95de8713b34e935c98b30a6cafb18a1dd901906a
David Morgan <david.alexandermorgan@gmail.com>
2018-09-22 22:09:16 +0000

    Added routes for jobs and submitted forms

---
+++ routes/jobs_router.js
@@ -0,0 +1,20 @@
+// Require Express and set router
+
+const express = require('express');
+const router = new express.Router();
+
+router.get('/jobs', (req, res) => {
+    
+})
+
+router.get('/jobs:id', (req, res) => {
+    
+})
+
+router.post('/jobs', (req, res) => {
+    
+})
+
+router.post('/jobs:id', (req, res) => {
+    
+})
---
+++ routes/submitted_forms_router.js
@@ -0,0 +1,20 @@
+// Require Express and set router
+
+const express = require('express');
+const router = new express.Router();
+
+router.get('/submitted_forms', (req, res) => {
+    
+})
+
+router.get('/submitted_forms:id', (req, res) => {
+    
+})
+
+router.post('/submitted_forms', (req, res) => {
+    
+})
+
+router.post('/submitted_forms:id', (req, res) => {
+    
+})
