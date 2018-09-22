db2bed044dc312f07b7cd4c5346ca65edd9fb733
James Dowell <james.s.dowell@gmail.com>
2018-09-22 22:05:51 +0000

    added form categories router

---
+++ routes/form_categories_router.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var formCategoriesRouter = express.Router();
+
+formCategoriesRouter
+  .get('/form_categories', (req, res) => {
+
+  })
+  
+  .post('/form_categories', (req, res) => {
+
+  })
+
+  .get('/form_categories/:id', (req, res) => {
+
+  })
+  
+  .post('/form_categories/:id', (req, res) => {
+
+  });
+
+  module.exports = formCategoriesRouter;
+  
