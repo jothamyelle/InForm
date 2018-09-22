f37545ad282297f6cb6b418e57cb85a160f20d8c
James Dowell <james.s.dowell@gmail.com>
2018-09-22 22:04:49 +0000

    added companies router

---
+++ routes/companies_router.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var companiesRouter = express.Router();
+
+companiesRouter
+  .get('/company/:id', (req, res) => {
+
+  })
+  
+  .post('/company/:id', (req, res) => {
+
+  })
+  
+  .post('/company', (req, res) => {
+
+  });
+
+  module.exports = companiesRouter;
+  
