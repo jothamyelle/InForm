3322da2b467b8bbfcc4af960efd743e01ca4dcd4
David Morgan <david.alexandermorgan@gmail.com>
2018-09-22 20:10:15 +0000

    First 6 seed files running successfully.

--- db/seeds/01_companies.js
+++ db/seeds/01_companies.js
@@ -10,8 +10,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "6554440175",
           email: "Jedediah88@yahoo.com",
           active: true,
-          date_created: new Date("Mon Dec 25 2017 14:16:44 GMT-0800 (PST)"),
-          date_updated: new Date("Sat Jun 29 2019 10:19:55 GMT-0700 (PDT)")
+          date_created: new Date("Mon Dec 25 2017 14:16:44 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Sat Jun 29 2019 10:19:55 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The cumque dolorum Company" , 
@@ -19,8 +19,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "4166215852",
           email: "Elroy.Jacobi24@yahoo.com",
           active: true,
-          date_created: new Date("Tue Apr 03 2018 11:03:30 GMT-0700 (PDT)"),
-          date_updated: new Date("Wed Apr 24 2019 12:56:47 GMT-0700 (PDT)")
+          date_created: new Date("Tue Apr 03 2018 11:03:30 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Wed Apr 24 2019 12:56:47 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The sed sint Company" , 
@@ -28,8 +28,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "6812370615",
           email: "Rosella.Goldner72@hotmail.com",
           active: true,
-          date_created: new Date("Fri Aug 03 2018 20:09:49 GMT-0700 (PDT)"),
-          date_updated: new Date("Wed Feb 20 2019 15:14:56 GMT-0800 (PST)")
+          date_created: new Date("Fri Aug 03 2018 20:09:49 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Wed Feb 20 2019 15:14:56 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The ut et Company" , 
@@ -37,8 +37,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "9437414490",
           email: "Berneice61@gmail.com",
           active: true,
-          date_created: new Date("Thu Sep 28 2017 14:46:07 GMT-0700 (PDT)"),
-          date_updated: new Date("Thu Jun 13 2019 06:12:13 GMT-0700 (PDT)")
+          date_created: new Date("Thu Sep 28 2017 14:46:07 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Thu Jun 13 2019 06:12:13 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The accusamus alias Company" , 
@@ -46,8 +46,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "5850938501",
           email: "Perry74@hotmail.com",
           active: true,
-          date_created: new Date("Sat Mar 10 2018 01:41:25 GMT-0800 (PST)"),
-          date_updated: new Date("Sat Aug 31 2019 09:28:15 GMT-0700 (PDT)")
+          date_created: new Date("Sat Mar 10 2018 01:41:25 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Sat Aug 31 2019 09:28:15 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The atque itaque Company" , 
@@ -55,8 +55,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "0455305054",
           email: "Jaeden_Bogisich37@gmail.com",
           active: true,
-          date_created: new Date("Wed Feb 28 2018 03:14:09 GMT-0800 (PST)"),
-          date_updated: new Date("Mon Nov 05 2018 13:25:25 GMT-0800 (PST)")
+          date_created: new Date("Wed Feb 28 2018 03:14:09 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Mon Nov 05 2018 13:25:25 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The et voluptates Company" , 
@@ -64,8 +64,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "5559956681",
           email: "Tyra53@yahoo.com",
           active: true,
-          date_created: new Date("Fri Jul 06 2018 06:57:40 GMT-0700 (PDT)"),
-          date_updated: new Date("Fri Jan 18 2019 08:59:33 GMT-0800 (PST)")
+          date_created: new Date("Fri Jul 06 2018 06:57:40 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Fri Jan 18 2019 08:59:33 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The vel non Company" , 
@@ -73,8 +73,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "3179568405",
           email: "Clarissa28@gmail.com",
           active: true,
-          date_created: new Date("Mon Feb 12 2018 17:15:55 GMT-0800 (PST)"),
-          date_updated: new Date("Sat Feb 09 2019 11:59:33 GMT-0800 (PST)")
+          date_created: new Date("Mon Feb 12 2018 17:15:55 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Sat Feb 09 2019 11:59:33 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The velit ratione Company" , 
@@ -82,8 +82,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "7217512230",
           email: "Fay_Paucek@yahoo.com",
           active: true,
-          date_created: new Date("Fri Jan 19 2018 00:05:56 GMT-0800 (PST)"),
-          date_updated: new Date("Tue Apr 02 2019 10:12:51 GMT-0700 (PDT)")
+          date_created: new Date("Fri Jan 19 2018 00:05:56 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Tue Apr 02 2019 10:12:51 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The veritatis ducimus Company" , 
@@ -91,8 +91,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "3489176035",
           email: "Murphy.Johns@gmail.com",
           active: true,
-          date_created: new Date("Sun Jan 28 2018 01:16:59 GMT-0800 (PST)"),
-          date_updated: new Date("Sun Feb 17 2019 03:00:46 GMT-0800 (PST)")
+          date_created: new Date("Sun Jan 28 2018 01:16:59 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Sun Feb 17 2019 03:00:46 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The libero unde Company" , 
@@ -100,8 +100,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "8800038250",
           email: "Edwin.Davis@hotmail.com",
           active: true,
-          date_created: new Date("Fri Mar 09 2018 09:38:31 GMT-0800 (PST)"),
-          date_updated: new Date("Tue Feb 26 2019 01:18:00 GMT-0800 (PST)")
+          date_created: new Date("Fri Mar 09 2018 09:38:31 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Tue Feb 26 2019 01:18:00 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The maiores ut Company" , 
@@ -109,8 +109,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "8320796321",
           email: "Edward_Homenick@gmail.com",
           active: true,
-          date_created: new Date("Fri Dec 01 2017 05:18:50 GMT-0800 (PST)"),
-          date_updated: new Date("Fri Sep 06 2019 12:42:58 GMT-0700 (PDT)")
+          date_created: new Date("Fri Dec 01 2017 05:18:50 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Fri Sep 06 2019 12:42:58 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The aut quia Company" , 
@@ -118,8 +118,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "2633440874",
           email: "Chadrick.West@gmail.com",
           active: true,
-          date_created: new Date("Mon Jun 11 2018 05:18:08 GMT-0700 (PDT)"),
-          date_updated: new Date("Sat Jul 20 2019 10:35:43 GMT-0700 (PDT)")
+          date_created: new Date("Mon Jun 11 2018 05:18:08 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Sat Jul 20 2019 10:35:43 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The optio et Company" , 
@@ -127,8 +127,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "7231947165",
           email: "Vinnie.Keeling@gmail.com",
           active: true,
-          date_created: new Date("Tue Jan 16 2018 13:57:59 GMT-0800 (PST)"),
-          date_updated: new Date("Thu May 02 2019 04:31:52 GMT-0700 (PDT)")
+          date_created: new Date("Tue Jan 16 2018 13:57:59 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Thu May 02 2019 04:31:52 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The amet ea Company" , 
@@ -136,8 +136,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "6164538549",
           email: "Richmond78@gmail.com",
           active: true,
-          date_created: new Date("Fri Mar 09 2018 05:28:23 GMT-0800 (PST)"),
-          date_updated: new Date("Tue Dec 04 2018 18:13:21 GMT-0800 (PST)")
+          date_created: new Date("Fri Mar 09 2018 05:28:23 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Tue Dec 04 2018 18:13:21 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The qui velit Company" , 
@@ -145,8 +145,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "0197872693",
           email: "Savion_Kozey@gmail.com",
           active: true,
-          date_created: new Date("Fri Jun 22 2018 09:22:35 GMT-0700 (PDT)"),
-          date_updated: new Date("Sun Jan 20 2019 23:18:26 GMT-0800 (PST)")
+          date_created: new Date("Fri Jun 22 2018 09:22:35 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Sun Jan 20 2019 23:18:26 GMT-0800 (PST)").toISOString()
         },
         {
           name: "The ut et Company" , 
@@ -154,8 +154,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "7942807731",
           email: "Kaden49@hotmail.com",
           active: true,
-          date_created: new Date("Tue Oct 10 2017 03:50:35 GMT-0700 (PDT)"),
-          date_updated: new Date("Wed Oct 10 2018 22:06:44 GMT-0700 (PDT)")
+          date_created: new Date("Tue Oct 10 2017 03:50:35 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Wed Oct 10 2018 22:06:44 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The doloremque quia Company" , 
@@ -163,8 +163,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "9101990583",
           email: "Daphney0@gmail.com",
           active: true,
-          date_created: new Date("Fri Nov 10 2017 15:09:22 GMT-0800 (PST)"),
-          date_updated: new Date("Sun Sep 08 2019 06:13:11 GMT-0700 (PDT)")
+          date_created: new Date("Fri Nov 10 2017 15:09:22 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Sun Sep 08 2019 06:13:11 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The nemo ut Company" , 
@@ -172,8 +172,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "4377223145",
           email: "Jovany56@gmail.com",
           active: true,
-          date_created: new Date("Fri Dec 15 2017 07:24:25 GMT-0800 (PST)"),
-          date_updated: new Date("Mon May 27 2019 00:44:11 GMT-0700 (PDT)")
+          date_created: new Date("Fri Dec 15 2017 07:24:25 GMT-0800 (PST)").toISOString(),
+          date_updated: new Date("Mon May 27 2019 00:44:11 GMT-0700 (PDT)").toISOString()
         },
         {
           name: "The aut ipsum Company" , 
@@ -181,8 +181,8 @@ exports.seed = function(knex, Promise) {
           phone_number: "7589026271",
           email: "Faye13@hotmail.com",
           active: true,
-          date_created: new Date("Sun May 06 2018 08:29:41 GMT-0700 (PDT)"),
-          date_updated: new Date("Sun Jun 16 2019 10:35:13 GMT-0700 (PDT)")
+          date_created: new Date("Sun May 06 2018 08:29:41 GMT-0700 (PDT)").toISOString(),
+          date_updated: new Date("Sun Jun 16 2019 10:35:13 GMT-0700 (PDT)").toISOString()
         }
       ]);
     });
--- db/seeds/05_form_categories.js
+++ db/seeds/05_form_categories.js
@@ -4,9 +4,9 @@ exports.seed = function(knex, Promise) {
       .then(function () {
         // Inserts seed entries
         return knex('form_categories').insert([
-            {name: 'Start of Shift', date_created: new Date("Wed Jan 31 2018 21:40:19 GMT-0800 (PST)"), date_updated: new Date("Fri Sep 21 2018 21:32:13 GMT-0700 (PDT)")},
-            {name: 'Mid Shift', date_created: new Date("Wed Apr 25 2018 18:07:42 GMT-0700 (PDT)"), date_updated: new Date("Sat Sep 22 2018 04:25:58 GMT-0700 (PDT)")},
-            {name: 'End of Shift', date_created: new Date("Sun Aug 12 2018 23:44:43 GMT-0700 (PDT)"), date_updated: new Date("Fri Sep 21 2018 20:55:18 GMT-0700 (PDT)")}
+            {name: 'Start of Shift', date_created: new Date("Wed Jan 31 2018 21:40:19 GMT-0800 (PST)").toISOString(), date_updated: new Date("Fri Sep 21 2018 21:32:13 GMT-0700 (PDT)").toISOString()},
+            {name: 'Mid Shift', date_created: new Date("Wed Apr 25 2018 18:07:42 GMT-0700 (PDT)").toISOString(), date_updated: new Date("Sat Sep 22 2018 04:25:58 GMT-0700 (PDT)").toISOString()},
+            {name: 'End of Shift', date_created: new Date("Sun Aug 12 2018 23:44:43 GMT-0700 (PDT)").toISOString(), date_updated: new Date("Fri Sep 21 2018 20:55:18 GMT-0700 (PDT)").toISOString()}
         ]);
       });
   };
