dc75dde171972ceef1b103d08861df181564ad81
James Dowell <james.s.dowell@gmail.com>
2018-09-23 21:53:00 +0000

    created helper function folder, added db helper file

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const ENV = process.env.ENV || "development";
 const express = require('express');
-const path = require('path');
-const knexConfig = require('./knexfile');
-const knex = require('knex')(knexConfig[ENV]);
-const knexLogger = require('knex-logger');
 const app = express();
+const path = require('path');
 
+const ENV = process.env.ENV || "development";
+
+const knexConfig = require('./knexfile');
+const knexLogger = require('knex-logger');
+const knex = require('knex')(knexConfig[ENV]);
+
+// Database helper functions
+const dbHelpers = require('./helper_functions/db.js')
+
+app.use(knexLogger(knex));
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
+// Api endpoint that returns a list of items
 app.get('/api/getEmployees', (req,res) => {
-    var employeeList = ["John Smith", "Dave M", "Jamie D", "Ben D", "Jotham Y"];
-    res.json(employeeList);
-    console.log('Sent list of employees');
+  var employeeList = ["John Smith", "Dave M", "Jamie D", "Ben D", "Jotham Y"];
+  res.json(employeeList);
+  console.log('Sent list of employees');
 });
 
-function getJobs() {
-    return knex.select().from('jobs')
-    .then(function(rows) {
-        console.log('knex getJobs', rows);
-        return rows;
-    });
-}
-
+// Api endpoint that returns a list of jobs
 app.get('/api/getJobs', (req,res) => {
-  getJobs().then(function(result) {
-      res.json(result);
-  console.log('Sent list of Jobs');
+  dbHelpers.getJobs().then(function(result) {
+    res.json(result);
+    console.log('Sent list of Jobs');
   });
 });
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 3005;
