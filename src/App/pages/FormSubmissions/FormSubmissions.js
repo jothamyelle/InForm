3933d36f75e7080439ecf2c6267136fd5fffb364
James Dowell <james.s.dowell@gmail.com>
2018-09-25 19:57:11 +0000

    setup forms submissions files

---
+++ src/App/pages/FormSubmissions/FormSubmissions.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import LoadingSpinner from '../Spinner';
+
+class FormSubmissions extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+  }
+}
+
+export default FormSubmissions;
