d39828a74fada52843c7bd623a6f6aa90bc0dc7d
David Morgan <david.alexandermorgan@gmail.com>
2018-09-25 18:03:33 -0700

    Update SearchResults.js

--- src/App/components/SearchResults.js
+++ src/App/components/SearchResults.js
@@ -36,22 +36,4 @@ class SearchResults extends Component {
   }
 }
 
-// const SearchResults = (props) => {
-//   const options = props.results.map(item => (
-//     <table key={item.id}>
-//       <thead>
-//         <th>ID</th>
-//         <th>Date</th>
-//         <th>User</th>
-//         <th>Form Name</th>
-//       </thead>
-//       <tbody>
-//       <td>{item.form_template_id}</td> 
-//       <td>{item.date_updated}</td>
-//       </tbody>
-//     </table>
-//   ))
-//   return <ul>{options}</ul>
-// }
-
 export default SearchResults
