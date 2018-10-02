4b2f1d54679df09a633e273d13c4fa38dc4829e8
James Dowell <james.s.dowell@gmail.com>
2018-10-02 02:17:25 +0000

    fixed adding and removing nodes from html head

--- src/App/pages/FormBuilder/FormBuilder.js
+++ src/App/pages/FormBuilder/FormBuilder.js
@@ -40,17 +40,51 @@ class FormBuilder extends Component {
       })
     })
   }
+
+  componentDidMount() {
+    // This adds style links to the head in order to style the form builder
+    let miuScript = document.createElement('script')
+    miuScript.setAttribute("src", "//cdn.muicss.com/mui-0.9.41/js/mui.min.js");
+    miuScript.setAttribute("id", "materialuiscript");
+    document.head.appendChild(miuScript);
+
+    let MUIStyle1 = document.createElement('link')
+    MUIStyle1.setAttribute("href", "//cdn.muicss.com/mui-0.9.41/css/mui.min.css");
+    MUIStyle1.setAttribute("id", "MUIStyle1");
+    MUIStyle1.setAttribute("type", "text/css");
+    MUIStyle1.setAttribute("rel", "stylesheet");
+    document.head.appendChild(MUIStyle1);
+
+    let MUIStyle2 = document.createElement('link')
+    MUIStyle2.setAttribute("href", "test.css");
+    MUIStyle2.setAttribute("id", "MUIStyle2");
+    MUIStyle2.setAttribute("type", "text/css");
+    MUIStyle2.setAttribute("rel", "stylesheet");
+    document.head.appendChild(MUIStyle2);
+
+    let robotoFont = document.createElement('link')
+    robotoFont.setAttribute("href", "https://fonts.googleapis.com/css?family=Roboto");
+    robotoFont.setAttribute("id", "robotoFont");
+    robotoFont.setAttribute("rel", "stylesheet");
+    document.head.appendChild(robotoFont);
+
+  }
+
+  componentWillUnmount() {
+    // Removing the style links when leaving form builder
+    let script = document.getElementById('materialuiscript')
+    let MUIStyle1 = document.getElementById('MUIStyle1')
+    let MUIStyle2 = document.getElementById('MUIStyle2')
+    let robotoFont = document.getElementById('robotoFont')    
+
+    document.head.removeChild(script)
+    document.head.removeChild(MUIStyle1)
+    document.head.removeChild(MUIStyle2)
+    document.head.removeChild(robotoFont)
+  }
   
   componentDidUpdate () {
-    if(this.state.newTemplateName && !this.state.redirect){
-      document.getElementsByTagName('head')[0].innerHTML +=
-      `
-      <link class="materialcssaddition" href="//cdn.muicss.com/mui-0.9.41/css/mui.min.css" rel="stylesheet" type="text/css" />
-      <link class="materialcssaddition" href="test.css" rel="stylesheet" type="text/css" />
-      <link class="materialcssaddition" href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-      <script class="materialcssaddition" src="//cdn.muicss.com/mui-0.9.41/js/mui.min.js"></script>
-      `
-      
+    if(this.state.newTemplateName && !this.state.redirect){      
       formBuilderObject.createFormBuilder(this.fbRef.current);
     }
   }
@@ -72,8 +106,8 @@ class FormBuilder extends Component {
           formContent
         })
         .then(() => {
-          formBuilderObject.emptyListOfDisplayOptions();
           document.getElementsByClassName('materialcssaddition').innerHTML = '';
+          formBuilderObject.emptyListOfDisplayOptions();
           this.setState({ newTemplateName: "", newTemplateCategory: "", formContent: "", redirect: true});
         }) 
       }
