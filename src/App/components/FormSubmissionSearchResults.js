b3d198659f72efd091be5cdc9f12c5dcd0b4d049
David Morgan <david.alexandermorgan@gmail.com>
2018-10-03 01:09:04 +0000

    hours footer working with searches and without

--- src/App/components/FormSubmissionSearchResults.js
+++ src/App/components/FormSubmissionSearchResults.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import FormSubmissionsStyles from '../pages/FormSubmissions/FormSubmissionsStyles.css'
 import Typography from '@material-ui/core/Typography';
 import { orange300 } from 'material-ui/styles/colors';
+import Footer from '../pages/Footer/Footer'
 
 import {
   Table,
@@ -17,13 +18,13 @@ class FormSubmissionSearchResults extends Component {
 
   render() {
     const options = this.props.results.map(item => (
-          <TableRow key={item.submitted_forms_id} displaySelectAll={false}>
-            <TableRowColumn style={{fontSize:17}}>{item.type}</TableRowColumn>
-            <TableRowColumn style={{fontSize:17}}>{item.job_name}</TableRowColumn>
-            <TableRowColumn style={{fontSize:17}}>{item.first_name} {item.last_name}</TableRowColumn>
-            <TableRowColumn style={{fontSize:17}}><FlatButton backgroundColor={orange300}>View</FlatButton></TableRowColumn>
-            <TableRowColumn style={{fontSize:17}}><FlatButton backgroundColor="lightgrey">Delete</FlatButton></TableRowColumn>
-          </TableRow>
+      <TableRow key={item.submitted_forms_id} displaySelectAll={false}>
+        <TableRowColumn style={{fontSize:17}}>{item.type}</TableRowColumn>
+        <TableRowColumn style={{fontSize:17}}>{item.job_name}</TableRowColumn>
+        <TableRowColumn style={{fontSize:17}}>{item.first_name} {item.last_name}</TableRowColumn>
+        <TableRowColumn style={{fontSize:17}}><FlatButton backgroundColor={orange300}>View</FlatButton></TableRowColumn>
+        <TableRowColumn style={{fontSize:17}}><FlatButton backgroundColor="lightgrey">Delete</FlatButton></TableRowColumn>
+      </TableRow>
     ))
 
     return (
@@ -31,19 +32,24 @@ class FormSubmissionSearchResults extends Component {
         <Typography variant="display2" gutterBottom align="center">
         Search Results
         </Typography>
-        <Table selectable={false} className={FormSubmissionsStyles.formsTable}>
-          <TableHeader displaySelectAll={false}>
-            <TableHeaderColumn style={{fontSize:30}}>Form Name</TableHeaderColumn>
-            <TableHeaderColumn style={{fontSize:30}}>Job</TableHeaderColumn>
-            <TableHeaderColumn style={{fontSize:30}}>Employee</TableHeaderColumn>
-            <TableHeaderColumn style={{fontSize:30}}>View</TableHeaderColumn>
-            <TableHeaderColumn style={{fontSize:30}}>Delete</TableHeaderColumn>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            {options}
-          </TableBody>
+        <div style={{height: "100vh"}}>
+          <Table selectable={false} className={FormSubmissionsStyles.formsTable}>
+            <TableHeader displaySelectAll={false}>
+              <TableHeaderColumn style={{fontSize:30}}>Form Name</TableHeaderColumn>
+              <TableHeaderColumn style={{fontSize:30}}>Job</TableHeaderColumn>
+              <TableHeaderColumn style={{fontSize:30}}>Employee</TableHeaderColumn>
+              <TableHeaderColumn style={{fontSize:30}}>View</TableHeaderColumn>
+              <TableHeaderColumn style={{fontSize:30}}>Delete</TableHeaderColumn>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              {options}
+            </TableBody>
           </Table>
-        </div>          
+        </div>
+        <div style={{position:"absolute", width: "100vw", left: "0"}}>
+          <Footer />
+        </div>
+      </div>          
       )
   }
 }
