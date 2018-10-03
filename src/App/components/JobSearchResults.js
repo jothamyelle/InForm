7837d401de73983952a30c9e9ea601f737f28182
Jotham Yelle <jothamyelle@hotmail.com>
2018-10-03 16:37:37 -0700

    Update JobSearchResults.js

--- src/App/components/JobSearchResults.js
+++ src/App/components/JobSearchResults.js
@@ -32,9 +32,9 @@ class JobSearchResults extends Component {
                   {this.props.jobs.map((item) => {
                     return (
                       <TableRow>
-                        <TableRowColumn style={{fontSize:24}}>{item.name}</TableRowColumn>
-                        <TableRowColumn style={{fontSize:24}}>{item.address}</TableRowColumn>
-                        <TableRowColumn style={{fontSize:24}}>{item.job_number}</TableRowColumn>
+                        <TableRowColumn style={{fontSize:25}}>{item.name}</TableRowColumn>
+                        <TableRowColumn style={{fontSize:25}}>{item.address}</TableRowColumn>
+                        <TableRowColumn style={{fontSize:25}}>{item.job_number}</TableRowColumn>
                       </TableRow>
                     );
                   })}
